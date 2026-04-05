0db3337678cc1c5f593b95464323c65128584cc0
aneykrap <aneykrap@gmail.com>
2026-04-05 16:05:37 +0900

    feat: 게시글 생성 컨트롤러 코드 수정

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -1,8 +1,12 @@
 package org.sopt.controller;
 
 import java.util.List;
+import org.sopt.dto.request.CreatePostRequest;
+import org.sopt.dto.response.CreatePostResponse;
+import org.sopt.service.PostService;
 
-class PostController {
+
+public class PostController {
     private final PostService postService = new PostService();
 
     // POST /posts
@@ -12,27 +16,5 @@ class PostController {
         } catch (IllegalArgumentException e) {
             return new CreatePostResponse(null, "🚫 " + e.getMessage());
         }
-    }
-
-    // GET /posts 📝 과제
-    public List<PostResponse> getAllPosts() {
-        // TODO: postService.getAllPosts() 호출해서 반환
-        return null;
-    }
-
-    // GET /posts/{id} 📝 과제
-    public PostResponse getPost(Long id) {
-        // TODO: postService.getPost(id) 호출, 예외 발생 시 null 반환
-        return null;
-    }
-
-    // PUT /posts/{id} 📝 과제
-    public void updatePost(Long id, String newTitle, String newContent) {
-        // TODO: postService.updatePost() 호출, 예외 발생 시 에러 메시지 출력
-    }
-
-    // DELETE /posts/{id} 📝 과제
-    public void deletePost(Long id) {
-        // TODO: postService.deletePost() 호출, 예외 발생 시 에러 메시지 출력
     }
 }
