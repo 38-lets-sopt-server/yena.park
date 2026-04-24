7ec0f1dd5945100fe0798f29afb8c6f13a6a0f8f
aneykrap <aneykrap@gmail.com>
2026-04-24 19:07:51 +0900

    refactor: SuccessStatus에 HttpStatus 추가 및 응답 구조 통일

--- src/main/java/org/sopt/common/code/SuccessStatus.java
+++ src/main/java/org/sopt/common/code/SuccessStatus.java
@@ -1,19 +1,27 @@
 package org.sopt.common.code;
 
+import org.springframework.http.HttpStatus;
+
 public enum SuccessStatus implements BaseCode {
 
-    POST_CREATE_SUCCESS("POST_201", "게시글 등록 성공"),
-    POST_READ_ALL_SUCCESS("POST_200", "게시글 전체 조회 성공"),
-    POST_READ_SUCCESS("POST_200", "게시글 단건 조회 성공"),
-    POST_UPDATE_SUCCESS("POST_200", "게시글 수정 성공"),
-    POST_DELETE_SUCCESS("POST_200", "게시글 삭제 성공");
+    POST_CREATE_SUCCESS(HttpStatus.CREATED, "POST_201", "게시글 등록 성공"),
+    POST_READ_ALL_SUCCESS(HttpStatus.OK, "POST_200", "게시글 전체 조회 성공"),
+    POST_READ_SUCCESS(HttpStatus.OK, "POST_200", "게시글 단건 조회 성공"),
+    POST_UPDATE_SUCCESS(HttpStatus.OK, "POST_200", "게시글 수정 성공"),
+    POST_DELETE_SUCCESS(HttpStatus.OK, "POST_200", "게시글 삭제 성공");
 
+    private final HttpStatus httpStatus;
     private final String code;
     private final String message;
 
-    SuccessStatus(String code, String message) {
+    SuccessStatus(HttpStatus httpStatus, String code, String message) {
+        this.httpStatus = httpStatus;
         this.code = code;
         this.message = message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
     }
 
     @Override
