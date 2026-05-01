aec3fbc35da9fe1b1dd2e45cf4365ada8ad9e75a
aneykrap <aneykrap@gmail.com>
2026-05-01 20:10:45 +0900

    feat : like code 추가

--- src/main/java/org/sopt/common/code/ErrorStatus.java
+++ src/main/java/org/sopt/common/code/ErrorStatus.java
@@ -5,7 +5,10 @@ public enum ErrorStatus implements BaseCode {
 
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, "POST_001", "게시글을 찾을 수 없습니다."),
     INVALID_INPUT(HttpStatus.BAD_REQUEST, "COMMON_001", "잘못된 요청입니다."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류");
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON_500", "서버 내부 오류"),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_404", "사용자를 찾을 수 없습니다."),
+    LIKE_ALREADY_EXISTS(HttpStatus.CONFLICT, "LIKE_001", "이미 좋아요를 눌렀습니다."),
+    LIKE_NOT_FOUND(HttpStatus.NOT_FOUND, "LIKE_002", "좋아요가 존재하지 않습니다.");
 
 
     private final HttpStatus httpStatus;
